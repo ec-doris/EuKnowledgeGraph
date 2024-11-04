03af54c48e4e843911166eb5571e9f967951b342
Vilmos Salamon <vilmos.salamon@gmail.com>
2024-11-04 10:31:34 +0100

    Update ECR

--- redeploy_image.sh
+++ redeploy_image.sh
@@ -1,3 +1,3 @@
-docker buildx build --platform linux/amd64 . -t 294118183257.dkr.ecr.eu-west-1.amazonaws.com/linkedopendata-wikibase-bundle:1.35.5-wmde.3 -f wikibase-bundle.Dockerfile
+docker buildx build --platform linux/amd64 . -t 891376987990.dkr.ecr.eu-west-1.amazonaws.com/linkedopendata-dev-wikibase-bundle:1.35.5-wmde.3 -f wikibase-bundle.Dockerfile
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 294118183257.dkr.ecr.eu-west-1.amazonaws.com
-docker push 294118183257.dkr.ecr.eu-west-1.amazonaws.com/linkedopendata-wikibase-bundle:1.35.5-wmde.3
+docker push 891376987990.dkr.ecr.eu-west-1.amazonaws.com/linkedopendata-dev-wikibase-bundle:1.35.5-wmde.3
